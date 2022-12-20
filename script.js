5ce12935cf66127c03c8fe4f6f04fb060a8b4b1b
Ashley <ashleyd3391@gmail.com>
2022-12-20 16:41:24 -0600

    Added function to allow user to input up to 5 numbers/ function to calculate results. Currently returns NaN error

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const operatorButtons = document.querySelectorAll(".operatorButton");
 
 //Display
 const screen = document.querySelector(".calcScreen");
+const screen2 = document.querySelector(".calcScreen2");
 
 //Operators
 const plus = document.querySelector("#plus");
@@ -11,6 +12,7 @@ const minus = document.querySelector("#minus");
 const times = document.querySelector("#times");
 const divide = document.querySelector("#divide")
 const equal = document.querySelector("#equal");
+    equal.addEventListener("click", calculate);
 const decimal = document.querySelector("#decimal");
 const sign = document.querySelector("#sign");
 const percent = document.querySelector("#percent");
@@ -18,19 +20,60 @@ const clear = document.querySelector("#clear");
 const deleteNumber = document.querySelector("#delete");
 
 //Values
-
+let previousValue = "";
+let currentValue = "";
+let operator = "";
 
 //Loop between buttons to display their text content when clicked
 
-numberButtons.forEach((button) =>
-    button.addEventListener('click', () => screen.innerHTML = (button.textContent))
-);
+    numberButtons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            updateNumber(e.target.textContent)
+            screen2.textContent = currentValue;
+        
+        });
+    });
 
-operatorButtons.forEach((button) =>
-    button.addEventListener('click', () => screen.innerHTML = (button.textContent))
-);
 
+    
+        operatorButtons.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                updateNumber(e.target.textContent);
+                screen2.textContent = previousValue + " " + operator;
+                screen.textContent = currentValue;
+            });
+        });
+
+function updateNumber(number){
+    if(currentValue.length <= 5) {
+        currentValue += number;
+        screen.textContent = currentValue;
+    }
+}
+
+
+function handleOperator(op) {
+    operator = op;
+    previousValue = currentValue;
+    currentValue = "";
+
+}
+
+
+function calculate(){
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    screen.textContent = previousValue += currentValue;
+    
+};
+
+
+
+//Resets calculator
+function reset(){
 clear.addEventListener("click", () => screen.innerHTML = "0");
+clear.addEventListener("click", () => screen2.innerHTML = "0");
+};
 
-
-equal.addEventListener("click", operate());
+reset();
